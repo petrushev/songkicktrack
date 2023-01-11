e0aeacd048f71ac46af0b3134b916e5df0aec55e
petrushev <b.petrushev@gmail.com>
2023-01-11 13:46:22 +0100

    Port to py3, delete old data, add Berlin

--- get_city.py
+++ get_city.py
@@ -7,13 +7,8 @@ from lxml.html import fromstring
 from dateutil.parser import parse as dt_parse
 
 
-BASE_URL = 'https://www.songkick.com/metro_areas/'
-
-
-
-def fetch_city(city):
+def print_city(city):
     page = 0
-    results = []
 
     while True:
         page = page + 1
@@ -22,48 +17,45 @@ def fetch_city(city):
         if len(page_results) == 0:
             break
 
-        results.extend(page_results)
+        print(format_results(page_results))
 
-        sleep(random() * .3 + .2)
-
-    return results
+        sleep(random() * .3 + .3)
 
 
 def fetch_page(city, page):
-    url = '{0}{1}?page={2}'.format(BASE_URL, city, page)
+    url = f"https://www.songkick.com/en/metro-areas/{city}?page={page}#metro-area-calendar"
+
     q = rq.get(url)
     doc = fromstring(q.content)
     doc.make_links_absolute(url)
 
-    items = doc.cssselect('li[title]')
+    dates = doc.cssselect('li.event-listings-element')
 
     results = []
 
-    for item in items:
-        dt_tag = item.cssselect('time[datetime]')[0]
-        dt = dt_parse(dt_tag.attrib['datetime'])
+    for date_el in dates:
+        date_info = date_el.cssselect('time')[0]
+        dt = dt_parse(date_info.attrib['datetime'])
 
-        title_tag = item.cssselect('p.artists.summary')[0]
-        title = title_tag.text_content().replace('\n', ' ').strip()
-        while ('   ' in title):
-            title = title.replace('   ', '  ')
+        link = date_el.cssselect('p.artists a.event-link')[0]
 
-        link_tag = title_tag.cssselect('a[href]')[0]
-        link = link_tag.attrib['href']
+        title = link.text_content().strip().replace('\n', ', ')
+        while '  ' in title:
+            title = title.replace('  ', ' ')
+        url = link.attrib['href']
 
-        result_item = {
+        results.append({
             'title': title,
-            'link': link,
-            'dt': dt}
-        results.append(result_item)
+            'link': link.attrib['href'],
+            'dt': dt
+        })
 
     return results
 
 
 def format_results(results):
     result = u'\n'.join(
-        [u'{0}\n    {1}\n    {2}\n'.format(
-            item['title'], item['link'], item['dt'].strftime('%Y %b %d'))
+        [f"{item['title']}\n    {item['link']}\n    {item['dt']:%Y %b %d}\n"
         for item in results]
     )
 
@@ -73,10 +65,7 @@ def format_results(results):
 def main():
     city = sys.argv[1]
 
-    results = fetch_city(city)
-    formatted = format_results(results)
-
-    print formatted.encode('utf-8')
+    results = print_city(city)
 
 
 if __name__ == '__main__':
